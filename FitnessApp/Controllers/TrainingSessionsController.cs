7c5676b982d1a4d92827abb423bb339a29165551
Vmakaveev <viktor.makaveev3@gmail.com>
2024-11-29 18:45:33 +0200

    Moved Controller

--- FitnessApp/Controllers/TrainingSessionsController.cs
+++ FitnessApp/Controllers/TrainingSessionsController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using FitnessApp.Data;
 using FitnessApp.Models;
 
-namespace FitnessApp
+namespace FitnessApp.Controllers
 {
     public class TrainingSessionsController : Controller
     {
