136cb303c99986b91bcca1b12adc010aab759187
Vmakaveev <viktor.makaveev3@gmail.com>
2024-12-02 22:28:20 +0200

    Admin dashboard.

--- FitnessApp/Controllers/AdminController.cs
+++ FitnessApp/Controllers/AdminController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
-namespace FitnessApp.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
