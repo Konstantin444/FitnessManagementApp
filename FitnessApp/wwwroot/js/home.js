aafa979617beb712f6a661fc6ec112bb0b431a72
Konstantin444 <kostadinov.main@gmail.com>
2024-12-06 13:10:28 +0200

    Home and nav

---
+++ FitnessApp/wwwroot/js/home.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const boxes = document.querySelectorAll(".info-box");
+
+    boxes.forEach((box, index) => {
+        setTimeout(() => {
+            if (index % 2 === 0) {
+                box.classList.add("slide-in-left");
+            } else {
+                box.classList.add("slide-in-right");
+            }
+        }, index * 300); // Delay each box's animation
+    });
+});
